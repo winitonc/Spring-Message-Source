e92324c42fa3806a2007f9ce14af22c54b5bef84
Winiton Chanapolchai <lumaster_2006@hotmail.com>
2017-11-13 14:32:14 +0700

    Init project

---
+++ src/main/java/com/chaowkung/spring/messagesoruce/MessagesoruceApplication.java
@@ -0,0 +1,27 @@
+package com.chaowkung.spring.messagesoruce;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+
+import java.util.Locale;
+
+@SpringBootApplication
+public class MessagesoruceApplication implements CommandLineRunner {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagesoruceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        String welcomeMessage = messageSource
+                .getMessage("welcome", new Object[]{"John", "Spring Message Source"}, Locale.getDefault());
+        System.out.println(welcomeMessage);
+    }
+}
---
+++ src/main/java/com/chaowkung/spring/messagesoruce/config/MessagesConfig.java
@@ -0,0 +1,20 @@
+package com.chaowkung.spring.messagesoruce.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+/**
+ * Created by Winiton Chanapolchai on 13/11/2560.
+ */
+@Configuration
+public class MessagesConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasename("messages");
+        return resourceBundleMessageSource;
+    }
+}
